fc71f482c46bf2f4661626a22352440967481420
AnishT2020 <anish.t2023@gmail.com>
2021-10-08 18:33:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+import sys
 
-#replace client_id and client_secret
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = '5801f1d7c9494b279259be2fe3d9aa72',
-                                                                              client_secret = 'ee018dc65b4c43fbbd6827534a64747a'))
+if len(sys.argv) > 1:
+    uri = sys.argv[1]
+else:
+    uri = 'spotify:artist:5K4W6rqBFWDnAN6FQUkS6x'
 
-artist_uri = 'spotify:artist:5K4W6rqBFWDnAN6FQUkS6x'
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = '5801f1d7c9494b279259be2fe3d9aa72',
+                                                                         client_secret = 'ee018dc65b4c43fbbd6827534a64747a'))
+response = sp.artist_top_tracks(uri)
 
-results = spotify.artist_albums(artist_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
-
-for album in albums:
-    print(album['name'])
+for track in response['tracks']:
+    print(track['name'])
