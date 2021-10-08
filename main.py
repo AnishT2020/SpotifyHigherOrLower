a7c8595c572e120ddb03974b93f0693e288231c3
AnishT2020 <anish.t2023@gmail.com>
2021-10-08 18:43:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import sys
+from pprint import pprint
 
 if len(sys.argv) > 1:
     uri = sys.argv[1]
@@ -10,6 +11,9 @@ else:
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = '5801f1d7c9494b279259be2fe3d9aa72',
                                                                          client_secret = 'ee018dc65b4c43fbbd6827534a64747a'))
 response = sp.artist_top_tracks(uri)
+artist = sp.artist(uri)
+pprint(response)
+
 
 for track in response['tracks']:
-    print(track['name'])
+    print(artist['name'] + ': ' + track['name'])
