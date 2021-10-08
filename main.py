abc2b9c453bb53fb65bdb1ce175f3c3bd9904813
AnishT2020 <anish.t2023@gmail.com>
2021-10-08 19:21:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ if len(sys.argv) > 1:
 else:
     uri = 'spotify:artist:5K4W6rqBFWDnAN6FQUkS6x'
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = '5801f1d7c9494b279259be2fe3d9aa72',
-                                                                         client_secret = 'ee018dc65b4c43fbbd6827534a64747a'))
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id = '',
+                                                                         client_secret = ''))
 response = sp.artist_top_tracks(uri)
 artist = sp.artist(uri)
 pprint(response)
